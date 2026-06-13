b06332d066449b89b00ce6e010c5f95b56f66313
Warthogandshark <bpalpha.brown@googlemail.com>
2026-06-13 07:02:35 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -37,7 +37,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL',
     'sqlite:///journal.db'
 )
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get(
+    'SECRET_KEY',
+    'local-development-key'
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
